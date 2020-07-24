1063bb6de9464eec381f1bac332b25da0e39edff
codeasm <nico.vijlbrief@outlook.com>
2020-07-24 21:44:00 +0200

    Modified build script for convenience, added a memory unit with FS (non working sofar)and build script now logs

--- build.sh
+++ build.sh
@@ -1,13 +1,46 @@
 #!/bin/sh
 #rm -rf PSXLinux
 #tar -xvf PSXLinux-kernel-2.4.x-beta1.tar.gz
+
+function build {
 cd linux
 make mrproper
 cp Config .config
 make dep
 make menuconfig
-make
+make 2>&1 | tee ../build.log
 cd ..
 tools/elf2psx/elf2psx -p linux/linux bin/kernel.exe
-exec wine ./tools/no\$psx/NO\$PSX.EXE bin/kernel.exe
+}
 
+function delete {
+rm bin/kernel.exe
+cd linux
+make mrproper
+rm 
+exit
+}
+
+function run {
+exec wine ./tools/no\$psx/NO\$PSX.EXE bin/kernel.exe
+}
+
+#----------------------------------------------------------------------
+#process commandline arguments
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -d|-delete|-deleteall)
+    delete
+    shift; # past argument and value
+    ;;-r|-run)
+    run
+    exit
+    shift; # past argument and value
+    ;;
+esac
+done
+
+build
+run
